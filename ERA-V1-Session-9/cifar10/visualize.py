8f816493a34b934eb9ca224e25c7595874c47d79
Niharika Vadapalli <subrahmanyamniharika@gmail.com>
2023-06-30 13:18:28 -0700

    mods

--- ERA-V1-Session-9/cifar10/visualize.py
+++ ERA-V1-Session-9/cifar10/visualize.py
@@ -69,10 +69,10 @@ def print_data_stats(data_loader):
     print(class_count)
     
 
-def print_train_log(train_acc, test_acc, train_loss, test_loss, learning_rates):
+def print_train_log(epochs, train_acc, test_acc, train_loss, test_loss, learning_rates):
     print("\nEpoch\t     Train Loss\t     Test Loss     Train Accuracy    Test Accuracy    Learning Rate")
     print("===========================================================================================")
-    for cnt in range(len(train_acc)):
-        print(f"{cnt+1}\t\t{train_loss[cnt]:0.2f}\t\t{test_loss[cnt]:0.2f}\t\t{train_acc[cnt]:0.2f}\t\t{test_acc[cnt]:0.2f}\t\t{learning_rates[cnt]:0.2f}\n")
+    for cnt in range(len(epochs)):
+        print(f"{cnt+1}\t\t{train_loss[cnt]:0.2f}\t\t{test_loss[cnt]:0.2f}\t\t{train_acc[cnt]:0.4f}\t\t{test_acc[cnt]:0.4f}\t\t{learning_rates[cnt]:0.8f}\n")
 
     print("===========================================================================================")
