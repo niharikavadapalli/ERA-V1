4835d921a580f63d82ebe2edbffc8c49e36a32b4
Niharika Vadapalli <subrahmanyamniharika@gmail.com>
2023-08-10 00:16:45 -0700

    mods

--- ERA-V1-Session-13/utils/config.py
+++ ERA-V1-Session-13/utils/config.py
@@ -9,7 +9,7 @@ DATASET_KAGGLE = '../input/pascal-voc-dataset-used-in-yolov3-video/PASCAL_VOC'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # seed_everything()  # If you want deterministic behavior
 NUM_WORKERS = 1
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 IMAGE_SIZE = 416
 NUM_CLASSES = 20
 LEARNING_RATE = 1e-5
