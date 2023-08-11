116b81c46720af7e0e7d92281fba91767ac7e830
Niharika Vadapalli <subrahmanyamniharika@gmail.com>
2023-08-11 02:10:32 -0700

    mods

--- ERA-V1-Session-13/utils/utils.py
+++ ERA-V1-Session-13/utils/utils.py
@@ -528,6 +528,27 @@ def plot_couple_examples(model, loader, thresh, iou_thresh, anchors):
         plot_image(x[i].permute(1,2,0).detach().cpu(), nms_boxes)
 
 
+def plot_couple_examples_test(model, loader, thresh, iou_thresh, anchors):
+    model.eval()
+    x, y = next(iter(loader))
+    x = x.to("cuda")
+    with torch.no_grad():
+        out = model(x)
+        bboxes = [[] for _ in range(x.shape[0])]
+        for i in range(3):
+            batch_size, A, S, _, _ = out[i].shape
+            anchor = anchors[i]
+            boxes_scale_i = cells_to_bboxes(
+                out[i], anchor, S=S, is_preds=True
+            )
+            for idx, (box) in enumerate(boxes_scale_i):
+                bboxes[idx] += box
+
+    for i in range(batch_size//4):
+        nms_boxes = non_max_suppression(
+            bboxes[i], iou_threshold=iou_thresh, threshold=thresh, box_format="midpoint",
+        )
+        plot_image(x[i].permute(1,2,0).detach().cpu(), nms_boxes)
 
 def seed_everything(seed=42):
     os.environ['PYTHONHASHSEED'] = str(seed)
